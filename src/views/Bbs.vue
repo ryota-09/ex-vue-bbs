e4694ad0a8d2415c13dd8c6f3afdb528c7e099f3
ryota-09 <n.ryo950911@gmail.com>
2022-02-16 12:23:31 +0900

    (1)記事一覧のみ表示

---
+++ src/views/Bbs.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    
+  </div>
+</template>
+
+<script lang="ts">
+import { Article } from "@/types/article";
+import { Component, Vue } from "vue-property-decorator";
+@Component
+export default class Bbs extends Vue {
+  //現在の記事一覧
+  private currentArticleList = new Array<Article>();
+  /**
+   * 記事一覧を表示する.
+   * @remarks Vuexストア内の記事一覧をcurrentArticleListに格納する
+   */
+  created(): void{
+    this.currentArticleList = this.$store.getters.getArticles;
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
